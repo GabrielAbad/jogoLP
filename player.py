c5db108b81702dc4e62098704f98b53c6f0a95a5
GabrielAbad <pedroabadg961@gmail.com>
2023-11-15 14:17:45 -0300

    Criei a classe player, que vai ser responsavel por tratar as colisões e as movimentações do boneco no jogo.

---
+++ player.py
@@ -0,0 +1,120 @@
+import pygame
+from config import Map, ScreenSettings, PlayerConfig,TilesConfig
+
+class Player(pygame.sprite.Sprite):
+
+    # Construtor da classe.
+    def __init__(self, player_img, row, column, platforms, blocks):
+
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite.__init__(self)
+
+        # Define estado atual
+        # Usamos o estado para decidir se o jogador pode ou não pular
+        self.state = PlayerConfig.STILL
+
+        # Ajusta o tamanho da imagem que representa o boneco
+        player_img = pygame.transform.scale(player_img, (PlayerConfig.PLAYER_WIDTH, PlayerConfig.PLAYER_HEIGHT))
+
+        # Define a imagem do sprite. Nesse exemplo vamos usar uma imagem estática (não teremos animação durante o pulo)
+        self.image = player_img
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+
+        # Guarda os grupos de sprites para tratar as colisões
+        self.platforms = platforms
+        self.blocks = blocks
+
+        # Posiciona o personagem
+        self.rect.x = column * TilesConfig.TILE_SIZE
+        self.rect.bottom = row * TilesConfig.TILE_SIZE
+
+        # Inicializa velocidades
+        self.speedx = 0
+        self.speedy = 0
+
+        # Define altura no mapa
+        # Essa variável sempre conterá a maior altura alcançada pelo jogador
+        # antes de começar a cair
+        self.highest_y = self.rect.bottom
+
+    def update(self):
+        self.__update_movement_y()
+        self.__check_block_colision()
+        self.__check_platform_colision()
+        self.__update_movement_x()
+        self.__check_horizontal_colision()
+
+    def __update_movement_y(self):
+        self.speedy += Map.GRAVITY
+        
+        if self.speedy > 0:
+            self.state = PlayerConfig.FALLING
+        
+        self.rect.y += self.speedy
+
+        if self.state != PlayerConfig.FALLING:
+            self.highest_y = self.rect.bottom
+        
+    def __check_block_colision(self):   
+        collisions = pygame.sprite.spritecollide(self, self.blocks, False)
+        
+        for collision in collisions:
+            
+            if self.speedy > 0:
+                self.rect.bottom = collision.rect.top
+                self.speedy = 0
+                self.state = PlayerConfig.STILL
+            
+            elif self.speedy < 0:
+                self.rect.top = collision.rect.bottom  
+                self.speedy = 0
+                self.state = PlayerConfig.STILL
+
+    def __check_platform_colision(self):
+        if self.speedy > 0: 
+            collisions = pygame.sprite.spritecollide(self, self.platforms, False)
+            
+            for platform in collisions:
+               
+                if self.highest_y <= platform.rect.top:
+                    self.rect.bottom = platform.rect.top
+                    self.highest_y = self.rect.bottom
+                    self.speedy = 0
+                    self.state = PlayerConfig.STILL
+
+    def __update_movement_x(self):
+        self.rect.x += self.speedx
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        elif self.rect.right >= ScreenSettings.WIDTH:
+            self.rect.right = ScreenSettings.WIDTH - 1 
+
+    def __check_horizontal_colision(self):
+        collisions = pygame.sprite.spritecollide(self, self.blocks, False)        
+        
+        for collision in collisions:            
+            if self.speedx > 0:
+                self.rect.right = collision.rect.left
+            elif self.speedx < 0:
+                self.rect.left = collision.rect.right
+
+    def jump(self):
+        # Só pode pular se ainda não estiver pulando ou caindo
+        if self.state == PlayerConfig.STILL:
+            self.speedy -= PlayerConfig.JUMP_SIZE
+            self.state = PlayerConfig.JUMPING
+
+    def walk_to_left(self):
+        self.speedx -= PlayerConfig.SPEED_X
+
+    def walk_to_right(self):
+        self.speedx += PlayerConfig.SPEED_X
+    
+    def stop_walk_left(self):
+        self.speedx += PlayerConfig.SPEED_X
+    
+    def stop_walk_right(self):
+        self.speedx -= PlayerConfig.SPEED_X
