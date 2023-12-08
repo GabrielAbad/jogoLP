77facf85755b0a9b54288d51be336ee66853c8b1
HenriqueBorges6 <carvalhoborgeshenrique@gmail.com>
2023-12-08 18:53:29 -0300

    using lavablock

--- app/assets.py
+++ app/assets.py
@@ -32,7 +32,7 @@ def load_assets(img_dir : str) -> dict:
     assets[PlayerConfig.FIREBOY_IMG] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'fireboypenut.png')).convert_alpha()
     assets[Map.BLOCK] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'tile-block.png')).convert()
     assets[Map.PLATF] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'tile-wood.png')).convert()
-    assets[Map.LAVA] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'lava.png')).convert()
+    assets[Map.LAVA] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'lavablock.png')).convert()
     assets[Map.WATER] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'water.jpg')).convert()
     assets[InitialScreenSettings.BACKGROUND_IMG] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'startscreampenut_resized.jpg')).convert()
     assets[EndScreenSettings.WIN_IMG] = pygame.image.load(path.join(path.dirname(__file__),img_dir, 'youwin.jpg')).convert()
